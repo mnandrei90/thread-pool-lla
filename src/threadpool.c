bb93250f71d603e2aa4e97e995a3501e997fff9c
Andrew M <mnandrei90@gmail.com>
2025-12-30 10:36:43 +0200

    Commit for tests

--- src/threadpool.c
+++ src/threadpool.c
@@ -1,57 +1,57 @@
-#include <pthread.h>
-
-#include "threadpool.h"
-
-void* thread_function(void* threadpool) {
-    threadpool_t* pool = (threadpool_t*)threadpool;
-
-    while (1) {
-        pthread_mutex_lock(&(pool->lock));
-
-        while (pool->queued == 0 && !pool->stop) {
-            pthread_cond_wait(&(pool->notify), &(pool->lock));
-        }
-
-        if (pool->stop) {
-            pthread_mutex_unlock(&(pool->lock));
-            pthread_exit(NULL);
-        }
-
-        task_t task = pool->task_queue[pool->queue_front];
-        pool->queue_front = (pool->queue_front + 1) % QUEUE_SIZE;
-        pool->queued--;
-
-        pthread_mutex_unlock(&(pool->lock));
-        (*(task.fn))(task.arg);
-    }
-
-    return NULL;
-}
-
-void threadpool_init(threadpool_t* pool) {
-    pool->queued = 0;
-    pool->queue_front = 0;
-    pool->queue_back = 0;
-    pool->stop = 0;
-
-    pthread_mutex_init(&(pool->lock), NULL);
-    pthread_cond_init(&(pool->notify), NULL);
-
-    for (int i = 0; i < THREADS; i++) {
-        pthread_create(&(pool->threads[i]), NULL, thread_function, pool);
-    }
-}
-
-void threadpool_destroy(threadpool_t *pool) {
-    pthread_mutex_lock(&(pool->lock));
-    pool->stop = 1;
-    pthread_cond_broadcast(&(pool->notify));
-    pthread_mutex_unlock(&(pool->lock));
-
-    for (int i = 0; i < THREADS; i++) {
-        pthread_join(pool->threads[i], NULL);
-    }
-
-    pthread_mutex_destroy(&(pool->lock));
-    pthread_cond_destroy(&(pool)->notify);
-}
+// #include <pthread.h>
+//
+// #include "threadpool.h"
+//
+// void* thread_function(void* threadpool) {
+//     threadpool_t* pool = (threadpool_t*)threadpool;
+//
+//     while (1) {
+//         pthread_mutex_lock(&(pool->lock));
+//
+//         while (pool->queued == 0 && !pool->stop) {
+//             pthread_cond_wait(&(pool->notify), &(pool->lock));
+//         }
+//
+//         if (pool->stop) {
+//             pthread_mutex_unlock(&(pool->lock));
+//             pthread_exit(NULL);
+//         }
+//
+//         task_t task = pool->task_queue[pool->queue_front];
+//         pool->queue_front = (pool->queue_front + 1) % QUEUE_SIZE;
+//         pool->queued--;
+//
+//         pthread_mutex_unlock(&(pool->lock));
+//         (*(task.fn))(task.arg);
+//     }
+//
+//     return NULL;
+// }
+//
+// void threadpool_init(threadpool_t* pool) {
+//     pool->queued = 0;
+//     pool->queue_front = 0;
+//     pool->queue_back = 0;
+//     pool->stop = 0;
+//
+//     pthread_mutex_init(&(pool->lock), NULL);
+//     pthread_cond_init(&(pool->notify), NULL);
+//
+//     for (int i = 0; i < THREADS; i++) {
+//         pthread_create(&(pool->threads[i]), NULL, thread_function, pool);
+//     }
+// }
+//
+// void threadpool_destroy(threadpool_t *pool) {
+//     pthread_mutex_lock(&(pool->lock));
+//     pool->stop = 1;
+//     pthread_cond_broadcast(&(pool->notify));
+//     pthread_mutex_unlock(&(pool->lock));
+//
+//     for (int i = 0; i < THREADS; i++) {
+//         pthread_join(pool->threads[i], NULL);
+//     }
+//
+//     pthread_mutex_destroy(&(pool->lock));
+//     pthread_cond_destroy(&(pool)->notify);
+// }
